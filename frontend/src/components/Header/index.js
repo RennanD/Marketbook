80e7cd5c9b61dcddc5a7511ef326e2a999a2086d
RennanD <rplaykl@gmail.com>
2019-12-23 11:14:50 -0300

    Configure redux

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { MdShoppingCart } from 'react-icons/md';
 
+import { connect } from 'react-redux';
+
 import logo from '../../assets/logo.svg';
 
 import { Container, Logo, Cart } from './styles';
 
-export default function Header() {
+function Header({ cart }) {
+    console.log(cart);
+
     return (
         <Container>
             <Link to="/">
@@ -18,10 +22,14 @@ export default function Header() {
             <Cart to="/cart">
                 <div>
                     <strong> Meu carrinho </strong>
-                    <span> 3 itens </span>
+                    <span> {cart.length} itens </span>
                 </div>
                 <MdShoppingCart size={36} color="#fefefe" />
             </Cart>
         </Container>
     );
 }
+
+export default connect(state => ({
+    cart: state.cart,
+}))(Header);
