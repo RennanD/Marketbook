f3d8a9a494f741799a4991001f10b9fa04ac1684
RennanD <rplaykl@gmail.com>
2019-12-25 16:32:18 -0300

    Configure feature to add product

--- mobile/src/store/modules/cart/sagas.js
+++ mobile/src/store/modules/cart/sagas.js
@@ -1,27 +1,31 @@
 import { all, call, select, takeLatest, put } from 'redux-saga/effects';
 
-import {
-    addToCartSuccess,
-    updateAmountSuccess,
-    updateAmountRequest,
-} from './actions';
+import { addToCartSuccess, updateAmountSuccess } from './actions';
 
 import { formartPrice } from '../../../utils/format';
 import api from '../../../services/api';
 
 function* addToCart({ id }) {
-    // const productExists = yield select(state =>
-    //     state.cart.find(p => p.id === id)
-    // );
+    const productExists = yield select(state =>
+        state.cart.find(p => p.id === id)
+    );
 
-    const response = yield call(api.get, `/products/${id}`);
+    const currentAmount = productExists ? productExists.amount : 0;
 
-    const data = {
-        ...response.data,
-        amount: 1,
-        priceFormatted: formartPrice(response.data.price),
-    };
-    yield put(addToCartSuccess(data));
+    const amount = currentAmount + 1;
+
+    if (productExists) {
+        yield put(updateAmountSuccess(id, amount));
+    } else {
+        const response = yield call(api.get, `/products/${id}`);
+
+        const data = {
+            ...response.data,
+            amount: 1,
+            priceFormatted: formartPrice(response.data.price),
+        };
+        yield put(addToCartSuccess(data));
+    }
 }
 
 function* updateAmount({ id, amount }) {
